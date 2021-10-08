3c8c057b95adbf55ef8f8473b2fd4890a565dc23
alipolat-js <84542348+alipolat-js@users.noreply.github.com>
2021-10-08 04:15:43 +0300

    environment vairables changed

--- src/api.js
+++ src/api.js
@@ -1,21 +1,21 @@
 import axios from "axios"
 
 export const searchMovie = async (query) => {
-  const {data} = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&page-1&include_adult=false&query=${query}`)
+  const {data} = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page-1&include_adult=false&query=${query}`)
   return data 
 }
 
 export const findMovie = async (id) => {
-  const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.API_KEY}&language=en-US&external_source=imdb_id`)
+  const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&external_source=imdb_id`)
   return data 
 }
 
 export const fetchTopRated  = async () => {
-  const {data} = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.API_KEY}&language=en-US&page=1`)
+  const {data} = await axios.get(`https://api.themoviedb.org/3/movie/top_rated?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
   return data
 }
 
 export const fetchPopularMovies  = async () => {
-  const {data} = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`)
+  const {data} = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
   return data
 }
